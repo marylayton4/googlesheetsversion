9502744b685519d39cadea372b6480e357bf3c85
Jennifer Wilson <jenniferwilson@Jennifers-MacBook-Pro.local>
2018-04-16 02:52:52 -0400

    first commit

---
+++ js/google-sheets-html-prog.js
@@ -0,0 +1,35 @@
+/*!
+ *
+ * Google Sheets To HTML v0.9a
+ *
+ * To use, simply replace the "tq?key=" value in the
+ * URL below with your own unique Google document ID
+ *
+ * The Google document's sharing must be set to public
+ *
+ */
+
+google.load('visualization', '1', {
+    packages: ['table']
+});
+var visualization;
+
+function drawVisualization() {
+    var query = new google.visualization.Query('https://spreadsheets.google.com/tq?key=198yAIX91beCgjZsZz3PXO_9mXKNq9tEpAqfEEw_14kw&output=html&usp=sharing');
+    query.setQuery('SELECT A, B, C, D, E label A "Program", B "Item", C "Quantity", D "Descritpion", E "History"');
+    query.send(handleQueryResponse);
+}
+
+function handleQueryResponse(response) {
+    if (response.isError()) {
+        alert('There was a problem with your query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+    }
+    var data = response.getDataTable();
+    visualization = new google.visualization.Table(document.getElementById('table'));
+    visualization.draw(data, {
+        allowHtml: true,
+        legend: 'bottom'
+    });
+}
+google.setOnLoadCallback(drawVisualization);
